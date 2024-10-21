45245d9b636dd01be402ee272379ed89281665da
unknown <ridlock@gmail.com>
2024-10-21 02:32:45 -0400

    updated file

--- LAMBDA/lambda_function.py
+++ LAMBDA/lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from workflow for CICD Lambda!')
+        'body': json.dumps('Hello from out of here for CICD Lambda!')
     }
 
 
